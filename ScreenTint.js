2d110b2abeb91cc509271450b63326b51293a738
caiofov <cfoviana@gmail.com>
2023-04-16 22:18:17 -0300

    feat: plugin description and params

--- ScreenTint.js
+++ ScreenTint.js
@@ -1,3 +1,27 @@
+/** /*:
+ * @plugindesc Alternative way of changing screen tone.
+ * @author Caio Oliveira
+ * 
+ * @help
+ * > Change screen tone directly via command:
+ * - tint tone <red> <green> <blue> <gray> <frames>
+ * 
+ * > Load saved tone:
+ * - tint load <saved tone identifier>
+ * @param Show logs
+ * @type boolean
+ * @on Display
+ * @off Do not display
+ * @default false
+ * @desc Whether if to display or not the plugin logs (visible by pressing F8 when the game is running). Useful for debugging.
+ * 
+ * @param Saved Tones
+ * @type struct<Tone>[]
+ * @desc Saved tones for loading in the game
+ * 
+ * 
+ */
+
 /** 
  /*~struct~Tone:
  * @param Name
