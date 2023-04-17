e99f28e8ea2d666aeeb66bac3817ffbc0c959d4e
caiofov <cfoviana@gmail.com>
2023-04-16 22:17:56 -0300

    feat: struct Tone for plugin params

---
+++ ScreenTint.js
@@ -0,0 +1,30 @@
+/** 
+ /*~struct~Tone:
+ * @param Name
+ * @type text
+ * @desc tone's identifier
+ * 
+ * @param Red
+ * @type number
+ * @min 0
+ * @max 255
+ * 
+ * @param Green
+ * @type number
+ * @min 0
+ * @max 255
+ * 
+ * @param Blue
+ * @type number
+ * @min 0
+ * @max 255
+ * 
+ * @param Gray
+ * @type number
+ * @min 0
+ * @max 255
+ * 
+ * @param Frames
+ * @type number
+ * @desc Number of frames for the transition (1s -> 60 frames)
+**/
